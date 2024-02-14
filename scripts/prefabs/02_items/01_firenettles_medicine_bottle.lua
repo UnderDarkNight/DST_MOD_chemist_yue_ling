e89384133018b1b59d4f4b57b0f22e5bf506ab7e
unknown <under.dark.night@outlook.com>
2024-02-14 20:05:24 +0800

    crash bug fix

--- scripts/prefabs/02_items/01_firenettles_medicine_bottle.lua
+++ scripts/prefabs/02_items/01_firenettles_medicine_bottle.lua
@@ -171,7 +171,7 @@ local function fn()
                 bullet:Hide()
                 local hide_dissq = 1.3 * 1.3
                 bullet.__dis_dsp_task = bullet:DoPeriodicTask(FRAMES,function()
-                    if bullet:GetDistanceSqToInst(inst) > hide_dissq then
+                    if bullet:GetDistanceSqToInst(attacker) > hide_dissq then
                         bullet.__dis_dsp_task:Cancel()
                         bullet:Show()
                     end
