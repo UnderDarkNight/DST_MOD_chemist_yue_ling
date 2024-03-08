8c6d1746d729fe9965c7b8f4b525334636effd72
unknown <under.dark.night@outlook.com>
2024-03-08 21:01:54 +0800

    beard container crash bug fix

--- scripts/prefabs/01_character/yue_ling_er_key_modules/01_beard_container_setup.lua
+++ scripts/prefabs/01_character/yue_ling_er_key_modules/01_beard_container_setup.lua
@@ -20,8 +20,12 @@ return function(inst)
         local beard_container_open_fn = function()
             local beard_container = inst.replica.inventory:GetEquippedItem(EQUIPSLOTS.BEARD)
             if beard_container then
-                if beard_container.components.container then
+                if beard_container.components.container and beard_container.replica.container then
                     beard_container.components.container:Open(inst)
+                else
+                    ----- 不知道为什么 会丢失 replica container 崩溃，强制重新给一个
+                    beard_container:Remove()
+                    inst.components.inventory:Equip(SpawnPrefab("chemist_other_beard_container"))
                 end
             else
                 inst.components.inventory:Equip(SpawnPrefab("chemist_other_beard_container"))
--- scripts/prefabs/09_spells/_excample.lua
+++ scripts/prefabs/09_spells/_excample.lua
@@ -3,35 +3,18 @@ local assets =
     -- Asset("ANIM", "anim/fireflies.zip"),
 }
 
-local function CastSpell(player)
-    local function CastSpell_p(player)
-        -- SpawnPrefab("shadow_bishop_fx").Transform:SetPosition(theplayer.Transform:GetWorldPosition()) 
-        player:SpawnChild("panda_casting_spell_fx")
-        local san = player.components.sanity:GetPercent()
-        local health = player.components.health:GetPercent()
-        player.components.sanity:SetPercent(health)
-        if san < 0.01 then
-            san = 0.01
-        end
-        player.components.health:SetPercent(san)
-    end
-    local flg,ret = pcall(CastSpell_p,player)    
-end
+
 
 local function builder_onbuilt(inst, builder)
     -- print(inst,builder,"fake error")
 	if builder then
-        local spell_name =  inst.prefab
-        if builder:HasTag("panda_fisherman") then
-            ----------------- 施法者判断并播放立绘
-            CastSpell(builder)
-            ------ 技能立绘
-        else
-            ------------------ 其它玩家只能使用一次
-            CastSpell(builder)
-            builder:PushEvent("Panda_Spell_Remove",{tag = spell_name})
-            --------------- 其它特效
-        end
+
+        local item = SpawnPrefab("chemist_item_exp_medicine3")
+
+
+        
+        builder.components.inventory:GiveItem(item)
+
     end
     inst:Remove()
 end
