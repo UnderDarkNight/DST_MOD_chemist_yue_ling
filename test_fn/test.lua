671446033aaad7644ca8c2e1283cd615f7d37bce
unknown <under.dark.night@outlook.com>
2024-03-08 17:32:54 +0800

    joystick visual mouse

--- test_fn/test.lua
+++ test_fn/test.lua
@@ -242,8 +242,114 @@ local flg,error_code = pcall(function()
             --     -- scale = 0.5,
             --     info_only = true,
             -- })
-            local debuff_inst = ThePlayer:GetDebuff("chemist_yue_ling_buff_revival_medicine_medicine")
-            print(debuff_inst)
+            -- local debuff_inst = ThePlayer:GetDebuff("chemist_yue_ling_buff_revival_medicine_medicine")
+            -- print(debuff_inst)
+    ----------------------------------------------------------------------------------------------------------------
+    ---- 鼠标模拟测试
+    
+        -- local mousepos = TheInput:GetScreenPosition()
+        -- print(mousepos.x, mousepos.y)
+       
+        -- print(TheInput:ControllerAttached())
+        -- local temp_TheInputProxy = getmetatable(TheInputProxy).__index
+        -- for k, v in pairs(temp_TheInputProxy) do
+        --     print(k,v,type(v))
+        -- end
+
+        ----------- 鼠标程序性移动
+            -- TheInputProxy:SetOSCursorPos(0,0)
+            -- local mx,my = TheInputProxy:GetOSCursorPos()
+            -- print(mx,my)
+            -- ThePlayer:DoTaskInTime(0.1,function()
+            --     for i = 1, 1000, 1 do
+            --     TheInput:OnMouseButton(i,true,mx,my)
+                    
+            --     end            
+            -- end)
+        ---------- 手柄按钮监听
+            --[[
+
+                CONTROL_MOVE_UP = 5  -- left joystick up
+                CONTROL_MOVE_DOWN = 6 -- left joystick down
+                CONTROL_MOVE_LEFT = 7 -- left joystick left
+                CONTROL_MOVE_RIGHT = 8 -- left joystick right
+
+
+                CONTROL_OPEN_INVENTORY = 45  -- right trigger
+                CONTROL_OPEN_CRAFTING = 46   -- left trigger
+                CONTROL_INVENTORY_LEFT = 47 -- right joystick left
+                CONTROL_INVENTORY_RIGHT = 48 -- right joystick right
+                CONTROL_INVENTORY_UP = 49 --  right joystick up
+                CONTROL_INVENTORY_DOWN = 50 -- right joystick down
+                CONTROL_INVENTORY_EXAMINE = 51 -- d-pad up
+                CONTROL_INVENTORY_USEONSELF = 52 -- d-pad right
+                CONTROL_INVENTORY_USEONSCENE = 53 -- d-pad left
+                CONTROL_INVENTORY_DROP = 54 -- d-pad down
+                CONTROL_PUTSTACK = 55
+                CONTROL_CONTROLLER_ATTACK = 56 -- X on xbox controller
+                CONTROL_CONTROLLER_ACTION = 57 -- A
+                CONTROL_CONTROLLER_ALTACTION = 58 -- B
+                CONTROL_USE_ITEM_ON_ITEM = 59
+            ]]--
+            -- if ThePlayer._temp_key_handler then
+            --     ThePlayer._temp_key_handler:Remove()
+            -- end
+            -- ThePlayer._temp_key_handler = TheInput:AddGeneralControlHandler(function(key,down)  ------ 30FPS
+            --     print(key,down)
+
+            -- end)
+        ------------
+            -- ThePlayer:DoTaskInTime(0.5,function()
+            --     -- TheInput:OnControl(MOUSEBUTTON_LEFT)
+
+            --     local mx,my = TheInputProxy:GetOSCursorPos()
+            --     print("mouse",mx,my)
+            --     -- TheInput:OnMouseMove(0,0)
+            --     TheInput:OnMouseButton(MOUSEBUTTON_LEFT,true,mx,my)
+            -- end)
+        ------------- TheFrontEnd
+            -- for k, v in pairs(TheFrontEnd) do
+            --     print(k,v,type(v))
+            -- end
+        -------------
+            ThePlayer:DoTaskInTime(0.5,function()
+            local crash_flag,reason = pcall(function()
+                print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                -- local mousepos = TheInput:GetScreenPosition()
+
+                local temp = TheInput:GetHUDEntityUnderMouse()
+
+
+                -- for k, v in pairs(temp.widget) do
+                --     print(k,v,type(v),tostring(v))
+                -- end
+                local temp_widget = temp.widget
+                local temp_button = nil
+                while true do
+                    print("current:",temp_widget,"parent:",temp_widget.parent)
+                    if tostring(temp_widget.parent) == "BUTTON" then
+                        temp_button = temp_widget.parent
+                    end
+                    temp_widget = temp_widget.parent
+                    if temp_widget == nil then
+                        break
+                    end
+
+                end
+                if temp_button and temp_button.OnControl then
+                    print("button:",temp_button.OnControl)
+                    temp_button:SetControl(MOUSEBUTTON_LEFT)    --- 设置激活按键为XXX
+                    temp_button:OnControl(MOUSEBUTTON_LEFT,true)
+                    temp_button:OnControl(MOUSEBUTTON_LEFT,false)
+                end
+                print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            end)
+
+            if not crash_flag then
+                print("Error ")
+                print(reason)
+            end
+        end)
     ----------------------------------------------------------------------------------------------------------------
     print("WARNING:PCALL END   +++++++++++++++++++++++++++++++++++++++++++++++++")
 end)
