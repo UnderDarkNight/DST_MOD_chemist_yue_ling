b53d364f53e5509f0a781ab5cc492673458e5483
unknown <under.dark.night@outlook.com>
2024-03-09 20:40:04 +0800

    character info upgrade

--- key_modules_for_chemist_yue_ling/01_character/01_chemist_yue_ling.lua
+++ key_modules_for_chemist_yue_ling/01_character/01_chemist_yue_ling.lua
@@ -27,26 +27,26 @@ end
 ---- 角色选择时候都文本
     if Language_check() == "ch" then
         -- The character select screen lines  --人物选人界面的描述
-        STRINGS.CHARACTER_TITLES["chemist_yue_ling"] = "药剂师·月玲儿"
-        STRINGS.CHARACTER_NAMES["chemist_yue_ling"] = "药剂师·月玲儿"
-        STRINGS.CHARACTER_DESCRIPTIONS["chemist_yue_ling"] = "XXXXXXXX"
-        STRINGS.CHARACTER_QUOTES["chemist_yue_ling"] = "XXXXXXXX"
+        STRINGS.CHARACTER_TITLES["chemist_yue_ling"] = "月玲儿"
+        STRINGS.CHARACTER_NAMES["chemist_yue_ling"] = "月玲儿"
+        STRINGS.CHARACTER_DESCRIPTIONS["chemist_yue_ling"] = "热爱药剂的可爱少女"
+        STRINGS.CHARACTER_QUOTES["chemist_yue_ling"] = "要不要来点药剂？"
 
         -- Custom speech strings  ----人物语言文件  可以进去自定义
         -- STRINGS.CHARACTERS[string.upper("chemist_yue_ling")] = require "speech_chemist_yue_ling"
 
         -- The character's name as appears in-game  --人物在游戏里面的名字
-        STRINGS.NAMES[string.upper("chemist_yue_ling")] = "药剂师·月玲儿"
-        STRINGS.SKIN_NAMES["chemist_yue_ling_none"] = "药剂师·月玲儿"  --检查界面显示的名字
+        STRINGS.NAMES[string.upper("chemist_yue_ling")] = "月玲儿"
+        STRINGS.SKIN_NAMES["chemist_yue_ling_none"] = "月玲儿"  --检查界面显示的名字
 
         --生存几率
-        STRINGS.CHARACTER_SURVIVABILITY["chemist_yue_ling"] = "特别容易"
+        STRINGS.CHARACTER_SURVIVABILITY["chemist_yue_ling"] = "容易？"
     else
         -- The character select screen lines  --人物选人界面的描述
         STRINGS.CHARACTER_TITLES["chemist_yue_ling"] = "Chemist Yue Ling Er"
         STRINGS.CHARACTER_NAMES["chemist_yue_ling"] = "Chemist Yue Ling Er"
-        STRINGS.CHARACTER_DESCRIPTIONS["chemist_yue_ling"] = "XXXXXXXX"
-        STRINGS.CHARACTER_QUOTES["chemist_yue_ling"] = "XXXXXXXX"
+        STRINGS.CHARACTER_DESCRIPTIONS["chemist_yue_ling"] = "Potions are the ladder of human progress"
+        STRINGS.CHARACTER_QUOTES["chemist_yue_ling"] = "Want to take some potions？"
 
         -- Custom speech strings  ----人物语言文件  可以进去自定义
         -- STRINGS.CHARACTERS[string.upper("chemist_yue_ling")] = require "speech_chemist_yue_ling"
@@ -73,5 +73,8 @@ end
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -----选人界面初始物品显示，物品相关的prefab
-    TUNING.GAMEMODE_STARTING_ITEMS.DEFAULT[string.upper("chemist_yue_ling")] = {"log"}
+    TUNING.GAMEMODE_STARTING_ITEMS.DEFAULT[string.upper("chemist_yue_ling")] = {"chemist_equipment_chemical_launching_gun",
+                                                                                                                                        "chemist_equipment_sublime_backpack",
+                                                                                                                                        "chemist_item_firenettles_medicine_bottle",
+                                                                                                                                        "chemist_item_cola_soda",}
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------
